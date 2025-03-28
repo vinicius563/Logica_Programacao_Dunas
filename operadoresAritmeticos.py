a161a611ad451f98e9e86e2274380cda97dcb1df
JOSE VINICIUS MARCELINO DE FREITAS <202408155654@alunos.unifanor.edu.br>
2025-03-28 10:04:50 -0300

    aritmetico

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,30 @@
+numero1 = 10
+numero2 = 20
+
+# Adição
+soma = numero1 + numero2
+print (soma)
+
+# Subtração
+subtracao = numero1 - numero2
+print(subtracao)
+
+# Multiplicação
+multiplicacao = numero1 * numero2
+print(multiplicacao)
+
+# Divisão
+divisao = numero1 / numero2
+print(divisao)
+
+# Potencia
+potencia = numero1 ** numero2
+print(potencia)
+
+# Modulo
+modulo = numero1 % numero2
+print(modulo)
+
+# Resto da divisao
+restodadivisao = numero1 // numero2
+print(restodadivisao)
