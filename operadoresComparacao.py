2d89648f4458d35db5c8efc7364a7572d3b4ad49
JOSE VINICIUS MARCELINO DE FREITAS <202408155654@alunos.unifanor.edu.br>
2025-03-28 10:50:11 -0300

    comparação

---
+++ operadoresComparacao.py
@@ -0,0 +1,20 @@
+x = 10
+y = 10
+
+# Comparação de igualdade 
+print(x == y)
+
+# Diferença
+print(x != y)
+
+# Superioridade
+print(x > y)
+
+# Inferioridade
+print(x < y)
+
+# Maior ou Igual
+print(x >= y)
+
+# Menor ou Igual 
+print(x <= y)
